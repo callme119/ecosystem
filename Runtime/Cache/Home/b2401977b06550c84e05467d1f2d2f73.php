6f43e0fcd0df969fc45f2eb537177e82cd6a2640
denghaoyang <275111108@qq.com>
2015-08-19 10:30:19 +0800

    更新插件

--- Runtime/Cache/Home/b2401977b06550c84e05467d1f2d2f73.php
+++ Runtime/Cache/Home/b2401977b06550c84e05467d1f2d2f73.php
@@ -3,22 +3,22 @@
 <head>
 	<meta charset="UTF-8">
 <title><?php echo C('WEB_SITE_TITLE');?></title>
-<link href="/ecosystem/public/static/bootstrap/css/bootstrap.css" rel="stylesheet">
-<link href="/ecosystem/public/static/bootstrap/css/bootstrap-responsive.css" rel="stylesheet">
-<link href="/ecosystem/public/static/bootstrap/css/docs.css" rel="stylesheet">
-<link href="/ecosystem/public/static/bootstrap/css/onethink.css" rel="stylesheet">
+<link href="/ecosystem/Public/static/bootstrap/css/bootstrap.css" rel="stylesheet">
+<link href="/ecosystem/Public/static/bootstrap/css/bootstrap-responsive.css" rel="stylesheet">
+<link href="/ecosystem/Public/static/bootstrap/css/docs.css" rel="stylesheet">
+<link href="/ecosystem/Public/static/bootstrap/css/onethink.css" rel="stylesheet">
 
 <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
 <!--[if lt IE 9]>
-<script src="/ecosystem/public/static/bootstrap/js/html5shiv.js"></script>
+<script src="/ecosystem/Public/static/bootstrap/js/html5shiv.js"></script>
 <![endif]-->
 
 <!--[if lt IE 9]>
-<script type="text/javascript" src="/ecosystem/public/static/jquery-1.10.2.min.js"></script>
+<script type="text/javascript" src="/ecosystem/Public/static/jquery-1.10.2.min.js"></script>
 <![endif]-->
 <!--[if gte IE 9]><!-->
-<script type="text/javascript" src="/ecosystem/public/static/jquery-2.0.3.min.js"></script>
-<script type="text/javascript" src="/ecosystem/public/static/bootstrap/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="/ecosystem/Public/static/jquery-2.0.3.min.js"></script>
+<script type="text/javascript" src="/ecosystem/Public/static/bootstrap/js/bootstrap.min.js"></script>
 <!--<![endif]-->
 <!-- 页面header钩子，一般用于加载插件CSS文件和代码 -->
 <?php echo hook('pageHeader');?>
@@ -31,7 +31,7 @@
 <div class="navbar navbar-inverse navbar-fixed-top">
     <div class="navbar-inner">
         <div class="container">
-            <a class="brand" href="<?php echo U('index/index');?>">OneThink</a>
+            <a class="brand" href="<?php echo U('index/index');?>">Ecosystem</a>
             <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
@@ -134,9 +134,9 @@
 <script type="text/javascript">
 (function(){
 	var ThinkPHP = window.Think = {
-		"ROOT"   : "/ecosystem/public", //当前网站地址
-		"APP"    : "/ecosystem/public/index.php?s=", //当前项目地址
-		"PUBLIC" : "/ecosystem/public/Public", //项目公共目录地址
+		"ROOT"   : "/ecosystem/Public", //当前网站地址
+		"APP"    : "/ecosystem/Public", //当前项目地址
+		"PUBLIC" : "/ecosystem/Public/Public", //项目公共目录地址
 		"DEEP"   : "<?php echo C('URL_PATHINFO_DEPR');?>", //PATHINFO分割符
 		"MODEL"  : ["<?php echo C('URL_MODEL');?>", "<?php echo C('URL_CASE_INSENSITIVE');?>", "<?php echo C('URL_HTML_SUFFIX');?>"],
 		"VAR"    : ["<?php echo C('VAR_MODULE');?>", "<?php echo C('VAR_CONTROLLER');?>", "<?php echo C('VAR_ACTION');?>"]
