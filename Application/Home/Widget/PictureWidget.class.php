b67f9db91f86efd57632f0abee50ba9ac4a96306
denghaoyang <275111108@qq.com>
2015-08-24 17:48:29 +0800

    更新首页幻灯片，把图片变为动态抓取

--- Application/Home/Widget/PictureWidget.class.php
+++ Application/Home/Widget/PictureWidget.class.php
@@ -2,6 +2,7 @@
 namespace Home\Widget;
 use Think\Controller;
 use Home\Model;
+use Home\Model\DocumentModel;
 
 /**
  * 图片widget
@@ -12,9 +13,21 @@ class PictureWidget extends Controller{
 	
 	/* 显示指定图片的URL */
 	public function getPictureUrl($id){
-		$pic = new Model\PictureModel;
-		echo $pic->getPictureUrlById($id);
-		return "hello";
+		//根据传入的分类信息取文档信息
+		$document = array();
+		$documentModel = new DocumentModel;
+		$document = $documentModel->lists($id);
+		//var_dump($document);
+
+		//根据文档信息取图片信息
+		$picture = array();
+		foreach ($document as $key => $value) {
+			$pic = new Model\PictureModel;
+			$picture[$key][path] = $pic->getPictureUrlById($value['cover_id']);
+			$picture[$key][title] = $value['description'];
+		}
+		$this->assign('picture', $picture);
+		$this->display('Public/sliderShow');
 	}
 	
 }
